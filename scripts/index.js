cc40314fc58ed9c9e382e7f7d3e852479f99c9bc
ashwin <sreaper37@gmail.com>
2021-03-26 22:38:41 +0530

    linking

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-input__text=document.querySelector("stream__name")
-
-document.querySelector(".btn").addEventListener("click",()=>{
-    
-    window.location.href="localhost:3000/game/" +input__Text.value;
-    console.log("xd");
-})
